826b51262d97abc34f8f2bb64547fe79a73e501c
w015319k <w015319k@A007-02>
2021-10-08 17:11:59 +0100

    Further comments and such

--- Application.h
+++ Application.h
@@ -1,5 +1,5 @@
 #pragma once
-
+//http://www.directxtutorial.com/Lesson.aspx?lessonid=11-4-1
 //Libraries used to create application
 // 
 //Makes it possible to create our window
@@ -23,9 +23,10 @@ struct SimpleVertex
     XMFLOAT4 Color;
 };
 
-//Constant Buffer
+//Constant Buffer - Talked about on line  61
 struct ConstantBuffer
 {
+	//We store these within since our matrices update in realtime  as game state changes , our view as we may move around the screen
 	XMMATRIX mWorld;
 	XMMATRIX mView;
 	XMMATRIX mProjection;
@@ -38,15 +39,17 @@ private:
 	HINSTANCE               _hInst;
 	HWND                    _hWnd;
 	
-	//Used to call relevant drives used to assist the creation of DX11 Application 
+	//Used to call relevant drivers used to assist the creation of DX11 Application 
 	D3D_DRIVER_TYPE         _driverType;
 	D3D_FEATURE_LEVEL       _featureLevel;
 
 
-	ID3D11Device*           _pd3dDevice;
-	ID3D11DeviceContext*    _pImmediateContext;
-	IDXGISwapChain*         _pSwapChain;
+	ID3D11Device*           _pd3dDevice;//Virtual Representation of your video adapter(virtual representation of graphics card)
+	ID3D11DeviceContext*    _pImmediateContext;//Applied as a device context that allows us to generate rendering commands to our device. Allowing us to manage our GPU and via that the rendering pipeline.
+	IDXGISwapChain*         _pSwapChain;//A series of buffers that allow us to take turn on buffers to render on , this variable is a pointer that references this process.
 	
+
+	//In order to render our application , we need to target our renderer , this is done so we can maintain the location in video memory to render into
 	ID3D11RenderTargetView* _pRenderTargetView;
 	
 	//Interfaces to relevant context , controls the specfic stage it is bound too
@@ -58,8 +61,7 @@ private:
 	//ID3D11Buffer* - This is a buffer interface that allows to access a buffer resource, this being unstructured memory , these buffers will allow us to store vertex or index data
 	ID3D11Buffer*           _pVertexBuffer; //Will store Vertex Data
 	ID3D11Buffer*           _pIndexBuffer; // Will store Index Data
-	ID3D11Buffer*           _pConstantBuffer; //This allows you to supply shader constants data to pipeline  ,such as textures , and our matrices that we want to update in real time.
-
+	ID3D11Buffer*           _pConstantBuffer; //This allows you to supply shader constants data to pipeline , data we want to continously supply 
 	//XMFLOAT4X4  - Structure that creates a 4*4 Floating Point Matrix , we can then use this to create the relevant world,view,projection matrices that we need.
 	XMFLOAT4X4              _world;
 	XMFLOAT4X4              _view;
